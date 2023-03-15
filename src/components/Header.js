e90c7b9943413ee992e318c71fab59438d3257c1
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-15 10:26:44 +0200

    Header muokkausta

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header>
-        
+    <header className='d-flex justify-content-between align-items-center'>
+      <div className="headerLeft">
+        <img src="https://media.discordapp.net/attachments/1084753750608072805/1085263140427669645/3valmis.png?width=1000&height=670" alt="" className="w-100" />
+      </div>
+      <div className="headerCenter">
+        <input type="text" name="Haku" id="" placeholder='Haku'  />
+      </div>
+      <div className="headerRight ">
+        Oikea
+      </div>
     </header>
   )
 }

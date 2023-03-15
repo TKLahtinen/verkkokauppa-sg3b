f3fab820a01ba92e54defc563a4684ff5bb449c4
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-15 14:54:12 +0200

    Haku bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,16 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='d-flex justify-content-between align-items-center'>
-      <div className="headerLeft">
+      <div className="headerLeft ms-3">
         <img src="https://cdn.discordapp.com/attachments/1084753750608072805/1085482794341384262/Logo.png" alt="" className="w-100" />
       </div>
-      <div className="headerCenter">
-        <input type="text" name="Haku" id="" placeholder='Haku'  />
+      <div className="headerCenter d-flex">
+        <input type="text" className="form-control me-3" placeholder='Haku' />
+        <button className="btn btn-light"> Hae </button>    
       </div>
-      <div className="headerRight ">
-        <button className="btn btn-primary">Sign In</button> <button className="btn btn-light">Log In</button>
+      <div className="headerRight me-3">
+        <button className="btn btn-primary">Sign In</button> 
+        <button className="btn btn-light">Log In</button>
       </div>
     </header>
   )

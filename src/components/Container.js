b996559ff3ffc10eea4d2a1b53d1951b5390e3b3
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-15 10:00:51 +0200

    Etusivu suunnitelu

---
+++ src/components/Container.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Container() {
+  return (
+    <div className='container '>
+      <div className="d-flex justify-content-center mt-3">
+        <img className='w-100' src="https://cdn.discordapp.com/attachments/1084753750608072805/1085160668040265738/kenka.png" alt="" />
+      </div>
+      <div className="d-flex flex-nowrap">
+        <div className="card w-25 m-3">
+          <img src="https://images.stockx.com/360/Air-Jordan-5-Retro-UNC-University-Blue/Images/Air-Jordan-5-Retro-UNC-University-Blue/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1678267312&h=384&q=57" alt="" className="card-img-top" />
+          <div className="card-body">
+            <h5>Jordan 5 Retro UNC University blue</h5>
+            <h3>€248</h3>
+          </div>
+        </div>
+        <div className="card w-25 m-3">
+          <img src="https://images.stockx.com/360/Air-Jordan-5-Retro-UNC-University-Blue/Images/Air-Jordan-5-Retro-UNC-University-Blue/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1678267312&h=384&q=57" alt="" className="card-img-top" />
+          <div className="card-body">
+            <h5>Jordan 5 Retro UNC University blue</h5>
+            <h3>€248</h3>
+          </div>
+        </div>
+        <div className="card w-25 m-3">
+          <img src="https://images.stockx.com/360/Air-Jordan-5-Retro-UNC-University-Blue/Images/Air-Jordan-5-Retro-UNC-University-Blue/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1678267312&h=384&q=57" alt="" className="card-img-top" />
+          <div className="card-body">
+            <h5>Jordan 5 Retro UNC University blue</h5>
+            <h3>€248</h3>
+          </div>
+        </div>
+        <div className="card w-25 m-3">
+          <img src="https://images.stockx.com/360/Air-Jordan-5-Retro-UNC-University-Blue/Images/Air-Jordan-5-Retro-UNC-University-Blue/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1678267312&h=384&q=57" alt="" className="card-img-top" />
+          <div className="card-body">
+            <h5>Jordan 5 Retro UNC University blue</h5>
+            <h3>€248</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

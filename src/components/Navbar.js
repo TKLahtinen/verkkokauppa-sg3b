0ffcae50a2cd91e74289b8d719d0e9619d2cd4a8
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-15 14:54:20 +0200

    Linkit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav></nav>
+    <nav className='d-flex justify-content-center align-items-center'>
+      <a href="" className='mx-3'>Tennarit</a>
+      <a href="" className='mx-3'>Kengät</a>
+      <a href="" className='mx-3'>Tarvikkeet</a>
+      <a href="" className='mx-3'>Muu</a>
+    </nav>
   )
 }

1d64b8fcabe7b00354dcf8df63ea9c49c366e3ee
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-29 10:19:45 +0300

    added react-router-dom and linked tennarit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Navbar() {
   return (
     <nav className='d-flex justify-content-center align-items-center'>
-      <a href="" className='mx-3'>Tennarit</a>
+      <Link className='mx-3' to="/Tennarit">Tennarit</Link>
       <a href="" className='mx-3'>Kengät</a>
       <a href="" className='mx-3'>Tarvikkeet</a>
       <a href="" className='mx-3'>Muu</a>

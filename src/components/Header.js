b6f77cbae1604b76037d1c3ea97bc122ab247048
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-29 09:08:51 +0300

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 
+
 export default function Header() {
   return (
     <header className='d-flex justify-content-between align-items-center'>
       <div className="headerLeft ms-3">
-        <img src="https://cdn.discordapp.com/attachments/1084753750608072805/1085482794341384262/Logo.png" alt="" className="w-100" />
+        <img src="https://cdn.discordapp.com/attachments/1084753750608072805/1085482794341384262/Logo.png" alt="logo" className="w-100" />
       </div>
       <div className="headerCenter d-flex">
         <input type="text" className="form-control me-3" placeholder='Haku' />

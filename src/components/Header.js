9a3162a3f7a4a0b78b013de11e203692ea220c10
TKLahtinen <n2lati00@students.oamk.fi>
2023-03-31 09:41:36 +0300

    Imported logo from files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/Logo.png'
 
 
 export default function Header() {
   return (
     <header className='d-flex justify-content-between align-items-center'>
       <Link className="headerLeft ms-3" to="*">
-        <img src="https://cdn.discordapp.com/attachments/1084753750608072805/1085482794341384262/Logo.png" alt="logo" className="w-100" />
+        <img src={logo} alt="logo" className="w-100" />
       </Link>
       <div className="headerCenter d-flex">
         <input type="text" className="form-control me-3" placeholder='Haku' />

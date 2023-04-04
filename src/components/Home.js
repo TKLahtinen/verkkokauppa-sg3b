eb6ff329799366ff47e4675ee182f2f3b4373eb1
TKLahtinen <n2lati00@students.oamk.fi>
2023-04-04 13:07:53 +0300

    Carousell rooting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import carousel1 from '../images/3beluga_rdy.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div className='container'>
-      <div className="d-flex justify-content-center mt-3">
+      <Link to="*" className="d-flex justify-content-center mt-3">
         <img className='w-100' src={carousel1} alt="" />
-      </div>
+      </Link>
       <div className="d-flex flex-nowrap">
         <div className="card w-25 m-3">
           <img src="https://images.stockx.com/360/Air-Jordan-5-Retro-UNC-University-Blue/Images/Air-Jordan-5-Retro-UNC-University-Blue/Lv2/img01.jpg?fm=avif&auto=compress&w=576&dpr=1&updated_at=1678267312&h=384&q=57" alt="" className="card-img-top" />

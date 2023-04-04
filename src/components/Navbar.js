4eb15a82e2a8e2c0464a64b850483e0d9b700eac
TKLahtinen <n2lati00@students.oamk.fi>
2023-04-04 13:08:17 +0300

    Added class for navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className='d-flex justify-content-center align-items-center'>
-      <Link className='mx-3' to="/Tennarit">Tennarit</Link>
-      <Link className='mx-3' to="/Kengat">Kengät</Link>
-      <a href="" className='mx-3'>Tarvikkeet</a>
-      <a href="" className='mx-3'>Muu</a>
+      <Link className='mx-3 navlink' to="/Tennarit">Tennarit</Link>
+      <Link className='mx-3 navlink' to="/Kengat">Kengät</Link>
+      <a href="" className='mx-3 navlink'>Tarvikkeet</a>
+      <a href="" className='mx-3 navlink'>Muu</a>
     </nav>
   )
 }

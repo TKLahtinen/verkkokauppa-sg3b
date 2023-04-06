c92db3f84f91897f3f8baab8d0ba636ebad3d117
TKLahtinen <n2lati00@students.oamk.fi>
2023-04-06 10:07:09 +0300

    Search bar working nicely

---
+++ phpbackend/search.php
@@ -0,0 +1,19 @@
+<?php
+require 'dbconnection.php';
+require 'headers.php';
+
+$dbcon = createDbConnection();
+
+$searchWord = $_GET['searchQuery'];
+$sql = "SELECT * FROM tuote WHERE nimi LIKE '%$searchWord%'";
+
+$statement = $dbcon->prepare($sql);
+$statement->execute();
+
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+header('HTTP/1.1 200 OK');
+$json = json_encode($rows);
+
+echo $json;
+
---
+++ src/components/Search.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import axios from 'axios'
+import { useEffect,useState } from 'react'
+import { Link,useParams } from 'react-router-dom';
+
+
+export default function Search() {
+
+    const { searchQuery } = useParams()
+    const URL = 'http://localhost:3001/phpbackend/search.php?searchQuery=';
+  
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios.get(URL + searchQuery)
+      .then((response) => {
+        const json = response.data;
+        console.log(json)
+        setItems(json)
+        console.log(items)
+        
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }, [searchQuery])
+
+  return (
+  <div className='container d-flex'>
+    <div className="row">
+      {items.map(item => (
+      <Link to={'/tuote/' + item.tuoteid} className="card m-3 w-25 p-0 productlink" key={item.id}>
+          <img src={item.img} class="card-img-top w-100 " alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{item.nimi} {item.hinta}€</h5>
+        </div>
+      </Link>
+      ))}
+    </div>
+  </div>
+  )
+}

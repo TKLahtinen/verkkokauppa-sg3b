3b1f6586af46e336318251c942f0bfc92add0041
s9rcie <jesse.ruonakoski@hotmail.com>
2023-04-19 19:25:16 +0300

    adjusted files for db

--- phpbackend/register.php
+++ phpbackend/register.php
@@ -6,14 +6,14 @@ require "usercontroller.php";
 $body = file_get_contents("php://input");
 $kayttajat = json_decode($body);
 
-if(!isset($kayttajat->id) || !isset($kayttajat->firstname) || !isset($kayttajat->lastname) || !isset($kayttajat->email) || !isset($kayttajat->pw)) {
+if(!isset($kayttajat->firstname) || !isset($kayttajat->lastname) || !isset($kayttajat->email) || !isset($kayttajat->pw)) {
     http_response_code(400);
     echo "Käyttäjää ei ole määritelty. Syötä oikeat tiedot";
     exit;
 }
 
 try {
-    registerUser($kayttajat->id, $kayttajat->firstname, $kayttajat->lastname, $kayttajat->email, $kayttajat->pw);
+    registerUser($kayttajat->firstname, $kayttajat->lastname, $kayttajat->email, $kayttajat->pw);
 } catch (Exception $e) {
     http_response_code(500);
     echo $e->getMessage();
--- phpbackend/usercontroller.php
+++ phpbackend/usercontroller.php
@@ -5,7 +5,7 @@ require "dbconnection.php";
 function registerUser($firstname, $lastname, $email, $pw) {
     $db = createDbConnection();
 
-    $sql = "INSERT INTO kayttajat VALUES (?,?,?,?,?)";
+    $sql = "INSERT INTO kayttajat VALUES (?,?,?,?)";
     $statement = $db->prepare($sql);
     $statement->execute(array($firstname, $lastname, $email, $pw));
 }

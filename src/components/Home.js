0bc06774622c58c680cd5f1445df119068545d3e
TKLahtinen <n2lati00@students.oamk.fi>
2023-04-19 16:03:22 +0300

    Home rollback

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,41 +7,23 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <>
-
-<div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <div className="carousel-caption">
-        <h1 className="display-2">Tarjous</h1>
-        <button type="button" className="btn btn-outline-light btn-lg">Tilaa tästä!</button>
+      <div className='container'>
+        <div className="row">
+          <div className="carousel slide ratio ratio-21x9" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <Link className="carousel-item active" to="/tuote/6">
+                <img src={carousel1} alt="..." className="d-block w-100" />
+              </Link>
+              <Link className="carousel-item" to="/tuote/13">
+                <img src={carousel2} alt="..." className="d-block w-100" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          Jottai
+        </div>
       </div>
-      <img src={carousel1} class="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <div className="carousel-caption">
-        <h1 className="display-2">Uutuus</h1>
-        <button type="button" className="btn btn-outline-light btn-lg">Ennakkotilaa tästä!</button>
-      </div>
-      <img src={carousel2} class="d-block w-100" alt="..."/>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-</div>
-
-
-
-</>
+    </>
   )
 }

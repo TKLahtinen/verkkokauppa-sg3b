de0c7b00870eb7eaa35a9f840e8130ad50361595
VAPAAAA <vaino.ruutikainen1@gmail.com>
2023-04-20 00:26:51 +0300

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary py-3 mt-3"><div className="container text-light text-center">
-<p className="display-6 mb-3">Ota yhteyttä</p>
-<div className="col-12 social padding">
+    <footer className="bg-secondary py-3 mt-3">
+		<div className="container text-light text-center">
+	<p className="display-6">Ota yhteyttä</p>
+	<div className="col-12 social padding">
 		<a href="#"><i className="bi bi-facebook"></i></a>
 		<a href="#"><i className="bi bi-twitter"></i></a>
 		<a href="#"><i className="bi bi-instagram"></i></a>
 		<a href="#"><i className="bi bi-youtube"></i></a>
-	</div>
-  </div>
+		</div>
+	</div>  
 </footer> 
   )
 }

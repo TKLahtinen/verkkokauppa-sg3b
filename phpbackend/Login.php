819476dad6da46b03b9d56b1cf41b3bb9f69fc28
TKLahtinen <n2lati00@students.oamk.fi>
2023-05-02 21:12:48 +0300

    backend koodit kirjautumiselle

---
+++ phpbackend/Login.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'headers.php';
+require 'functions.php';
+
+$body = file_get_contents("php://input");
+$user = json_decode($body);
+
+if(!isset($user->username) || !isset($user->password)){
+    http_response_code(400);
+    echo "Käyttäjätunnus tai salasana puuttuu";
+    exit;
+}
+
+try{
+    $data = loginUser($user->username, $user->password);
+    echo $data;
+}catch(Exception $e){
+    http_response_code(500);
+    echo $e->getMessage();
+    exit;
+}
--- phpbackend/functions.php
+++ phpbackend/functions.php
@@ -1,2 +1,21 @@
 <?php
 
+require "dbconnection.php";
+
+function registerUser($username, $password) {
+    $dbcon = createDbConnection();
+
+    $sql = "INSERT INTO users (username, pw) VALUES (?,?)";
+
+    $statement = $dbcon->prepare($sql);
+    $statement->execute(array($username, $password));
+}
+
+function loginUser($username, $password) {
+    $dbcon = createDbConnection();
+    $sql = "SELECT id, username, rights FROM users WHERE username = ? AND pw = ?";
+    $statement = $dbcon->prepare($sql);
+    $statement->execute(array($username, $password));
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return json_encode($data);
+}

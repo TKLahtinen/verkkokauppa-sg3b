beddda4f2190fc42b561ff55e14af163c069a3ad
TKLahtinen <n2lati00@students.oamk.fi>
2023-05-02 19:30:36 +0300

    Changed links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,13 @@ export default function Header() {
   return (
 
     <div className='header'>
-    <Link className="logolink" to="*">
+    <Link className="logolink" to="/">
         <img src={logo} alt="logo"/>
       </Link>
     <ul className="icon-area">
       <li className='icon-item'><Link className="icon-link" to="*"><i class="bi bi-door-open-fill"></i>Kirjaudu sisään</Link></li>
       <li className='icon-item'><Link className="icon-link" to="*"><i class="bi bi-box-arrow-in-right"></i>Rekisteröidy</Link></li>
-      <li className='icon-item'><Link className='icon-link'to="*"><i class="bi bi-cart-fill"></i>Ostoskori</Link></li>
+      <li className='icon-item'><Link className='icon-link'to="/tilaus"><i class="bi bi-cart-fill"></i>Ostoskori</Link></li>
     </ul>
     </div>
   )

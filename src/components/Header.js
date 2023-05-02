f6f6a0c77a61a61866736fa00f8829850d526161
VAPAAAA <vaino.ruutikainen1@gmail.com>
2023-05-02 06:28:14 +0300

    Paranneltu Header komponenttia Lisätty kuva register.png log-/signin toiminnoille. Siirretty Hakukenttä Header -> Navbar
    Paranneltu Navbar komponenttia
    Lisätty Navbar "teksti logo"
    Lisätty Hakukenttä
    Mobiilissa Navbar puskee sisällön alaspäin, eikä tule sisällön päälle.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,20 @@ import React from 'react'
 import { useRef,useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/Logo.png'
+import register from '../images/register.png'
 
 
 export default function Header() {
 
-  const searchInput = useRef()
-
-  function handleSearch(e) {
-    e.preventDefault()
-    const searchQuery = searchInput.current.value
-    console.log(searchQuery)
-    window.location.href = '/haku/' + searchQuery
-    
-  }
-
   return (
-    <header className='d-flex justify-content-between align-items-center'>
-      <Link className="headerLeft ms-3" to="*">
-        <img src={logo} alt="logo" className="w-100" />
+    <div className='header'>
+    <Link className="logolink" to="*">
+        <img src={logo} alt="logo"/>
       </Link>
-      <div className="headerCenter d-flex">
-        <input type="text" className="form-control me-3" placeholder='Haku' ref={searchInput} />
-        <button className="btn btn-light" onClick={handleSearch}> Hae </button>
-      </div>
-      <div className="headerRight me-3">
-        <button className="btn btn-primary">Sign In</button> 
-        <button className="btn btn-light">Log In</button>
-      </div>
-    </header>
+    <ul className="icon-area">
+      <li className='icon-item'><Link className="icon-link" to="*"><img src={register}/>Kirjaudu sisään</Link></li>
+      <li className='icon-item'><Link className="icon-link" to="*"><img src={register}/>Rekisteröidy</Link></li>
+    </ul>
+    </div>
   )
 }

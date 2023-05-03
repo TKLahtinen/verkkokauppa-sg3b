715a8cb800b41a8affbad61aee97aeee5c072150
TKLahtinen <n2lati00@students.oamk.fi>
2023-05-03 12:46:00 +0300

    login and signup works

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,13 @@ import logo from '../images/Logo.png'
 
 export default function Header() {
 
+  const userData = JSON.parse(sessionStorage.getItem('userData'))
+
+  function handleLogout() {
+    sessionStorage.removeItem('userData');
+    window.location.reload();
+  }
+
   return (
 
     <div className='header'>
@@ -13,9 +20,42 @@ export default function Header() {
         <img src={logo} alt="logo"/>
       </Link>
     <ul className="icon-area">
-      <li className='icon-item'><Link className="icon-link" to="*"><i class="bi bi-door-open-fill"></i>Kirjaudu sisään</Link></li>
-      <li className='icon-item'><Link className="icon-link" to="*"><i class="bi bi-box-arrow-in-right"></i>Rekisteröidy</Link></li>
-      <li className='icon-item'><Link className='icon-link'to="/tilaus"><i class="bi bi-cart-fill"></i>Ostoskori</Link></li>
+    {userData ? (
+        <>          
+          <li className='icon-item'>
+            <button className='icon-link' onClick={handleLogout}>
+              Log out
+            </button>
+          </li>
+          <li className='icon-item'>
+            <Link className='icon-link' to='/'>
+            <i className="bi bi-person-circle"></i>
+              {userData[0].username}
+            </Link>
+          </li>
+        </>
+        ) : (
+          <>
+            <li className='icon-item'>
+              <Link className="icon-link" to="/login">
+                <i className="bi bi-door-open-fill"></i>
+                Kirjaudu sisään
+              </Link>
+            </li>
+            <li className='icon-item'>
+              <Link className="icon-link" to="/signup">
+                <i className="bi bi-box-arrow-in-right"></i>
+                Rekisteröidy
+              </Link>
+            </li>
+          </>
+        )}
+        <li className='icon-item'>
+          <Link className='icon-link' to="/tilaus">
+            <i className="bi bi-cart-fill"></i>
+            Ostoskori
+          </Link>
+        </li>
     </ul>
     </div>
   )
---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { set, useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useState } from 'react';
+
+export default function Login({handleLogin, msg}) {
+
+    const {register, handleSubmit} = useForm();
+
+  return (
+    <div className='container'>
+        <form onSubmit={handleSubmit(handleLogin)}>
+            <div className='m-3'>
+                <label className="form-label">Käyttäjänimi</label>
+                <input className='form-control' {...register('username')}/>
+                <label className="form-label">Salasana</label>
+                <input className='form-control' type='password' {...register('password')}/>
+        
+            </div>
+            <button type="submit" class="btn btn-primary">Kirjaudu</button>
+        </form>
+        <span>{msg}</span>
+    </div>
+  )
+}
